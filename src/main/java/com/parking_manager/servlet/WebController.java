526b56649b8cbbe852dfc3cb0a92aca54d3dc9dc
amorro <44436902+amorro@users.noreply.github.com>
2020-02-01 00:34:04 +0100

    a

---
+++ src/main/java/com/parking_manager/servlet/WebController.java
@@ -0,0 +1,9 @@
+package com.parking_manager.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class WebController extends HttpServlet {
+
+}
---
+++ target/parking-manager-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<jsp:include page="common/header.jsp"/>
+
+<h1>Title</h1>
+
+
+
+
+<jsp:include page="common/footer.jsp"/>
